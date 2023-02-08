68fcb4929fbd2db9195149eca5b3447246a47c02
Luke Admans <lukeacreative@gmail.com>
2023-02-08 12:51:29 +0000

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,36 @@
 import React, { useRef, useState } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
+import { Model } from './Model'
 
 import './styles/global.css'
 
-function Box(props) {
+function ModelComponent(props) {
 	// This reference will give us direct access to the mesh
 	const mesh = useRef()
 	// Set up state for the hovered and active state
 	const [hovered, setHover] = useState(false)
 	const [active, setActive] = useState(false)
 	// Subscribe this component to the render-loop, rotate the mesh every frame
-	useFrame((state, delta) => (mesh.current.rotation.x += delta))
+	useFrame((state, delta) => (mesh.current.rotation.y += delta))
 	// Return view, these are regular three.js elements expressed in JSX
 	return (
-		<mesh {...props} ref={mesh} scale={active ? 1.5 : 1} onClick={event => setActive(!active)} onPointerOver={event => setHover(true)} onPointerOut={event => setHover(false)}>
-			<boxGeometry args={[1, 1, 1]} />
-			<meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+		<mesh {...props} ref={mesh} scale={0.25}>
+			{/* <boxGeometry args={[1, 1, 1]} /> */}
+			<Model />
+			{/* <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} /> */}
 		</mesh>
 	)
 }
 
 function App() {
 	return (
-		<div className='App'>
-			<Canvas>
+		<div className='App' style={{ height: '100vh' }}>
+			{/* <h1>Geez</h1> */}
+			<Canvas height={'100vh'}>
 				<ambientLight />
 				<pointLight position={[10, 10, 10]} />
-				<Box position={[-1.2, 0, 0]} />
-				<Box position={[1.2, 0, 0]} />
+				{/* <Box position={[-1.2, 0, 0]} /> */}
+				<ModelComponent position={[0, -1.5, 0]} />
 			</Canvas>
 		</div>
 	)
